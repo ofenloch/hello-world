dca6273c2b35368dd5e3c2429746861f5fdb3eac
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2022-08-18 18:13:41 +0000

    add another test

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -7,6 +7,8 @@ namespace Library.UnitTests
 
     public class UnitTest1
     {
+
+        // The [Fact] attribute declares a test method that's run by the test runner. From the PrimeService.Tests folder, run dotnet test.
         [Fact]
         public void Test1()
         {
@@ -14,8 +16,25 @@ namespace Library.UnitTests
             double b = 31;
             double expectedValue = a + b;
             double actualValue = Library.MyMath.Add(a, b);
-            Assert.True(expectedValue == actualValue , $"actual value {actualValue} should be equal to {expectedValue}");
+            Assert.True(expectedValue == actualValue, $"actual value {actualValue} should be equal to {expectedValue}");
         }
+    
+        // [Theory] represents a suite of tests that execute the same code but have different input arguments.
+        // [InlineData] attribute specifies values for those inputs.
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = Library.MyMath.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+
+
+
     }
 
 } // namespace Library.UnitTests
