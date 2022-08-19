9833252e9fc5251c01fd2bcd2f1891502679c747
Oliver Ofenloch <57812959+ofenloch@users.noreply.github.com>
2022-08-19 04:19:56 +0000

    make logging a bit more realistic

--- src/HelloWorld.cs
+++ src/HelloWorld.cs
@@ -13,23 +13,26 @@ namespace MyApp
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            Logger.Info("Starting program ...");
+            string LOGDIR = Environment.GetEnvironmentVariable("LOGDIR");
+            Logger.Debug("LOGDIR is set to {0}", LOGDIR);
 
-            string LOGDIR=Environment.GetEnvironmentVariable("LOGDIR");
-            Console.WriteLine(String.Format("LOGDIR is {0}", LOGDIR));
-
-            Logger.Trace("Trace");
-            Logger.Debug("Debug");
-            Logger.Info("Info");
-            Logger.Warn("Warn");
-            Logger.Error("Error");
-            Logger.Fatal("Fatal");
+            // Just for testing:
+            // Logger.Trace("Trace");
+            // Logger.Debug("Debug");
+            // Logger.Info("Info");
+            // Logger.Warn("Warn");
+            // Logger.Error("Error");
+            // Logger.Fatal("Fatal");
 
 
             Console.WriteLine("Hello, World!");
 
             double x = 1.234;
             double y = 4.321;
+            Logger.Debug("calling Library.MyMath.Add(x, y) with x={0} and y={1} ...", x, y);
             double sum = Library.MyMath.Add(x, y);
+            Logger.Debug("calling Library.MyMath.Multiply(x, y) with x={0} and y={1} ...", x, y);
             double prod = Library.MyMath.Multiply(x, y);
             Console.WriteLine(String.Format("{0} plus {1} makes {2}", x, y, sum));
             Console.WriteLine(String.Format("{0} times {1} makes {2}", x, y, prod));
@@ -47,17 +50,20 @@ namespace MyApp
             PrintElement(myData, 101);
             PrintElement(myData, 102);
 
+            Logger.Info("Terminating program ...");
         }
         public static void PrintElement(Library.DataStore<int, string> Store, int index)
         {
             Library.Pair<int, string>? element = Store.GetElementByIndex(index);
             if (element is Library.Pair<int, string> valueOfElment)
             {
+                Logger.Trace("idx {0}: found element", index);
                 Console.WriteLine(String.Format("idx {0}: key {1}, value {2}", index,
                     element.GetKey(), element.GetValue()));
             }
             else
             {
+                Logger.Warn("idx {0}: no such element in DataStore", index);
                 Console.WriteLine(String.Format("idx {0}: no such element in DataStore", index));
             }
         }
